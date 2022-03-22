19b17bed3a82208c398f0ec6ed81d32e4e34a98e
Niko Sardas <nikosardas.dev@gmail.com>
2022-03-22 15:13:59 -0400

    celtic like frantal setup

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
   canvas.width = 500
   canvas.height = 500
 
-  ctx.lineWidth = 8
+  ctx.lineWidth = 30
   ctx.lineCap = 'round'
   ctx.strokeStyle = 'lightblue'
   ctx.fillStyle = 'blue'
@@ -15,8 +15,11 @@ window.addEventListener('load', function () {
       this.canvasWidth = canvasWidth
       this.canvasHeight = canvasHeight
       this.size = canvasWidth * 0.2
-      this.sides = 15
-      this.maxLevel = 10
+      this.sides = 4
+      this.maxLevel = 7
+      this.scale = 0.6
+      this.angle = 1
+      this.branches = 1
     }
     draw(context) {
       context.save()
@@ -24,24 +27,36 @@ window.addEventListener('load', function () {
       context.scale(1, 1)
 
       for (let i = 0; i < this.sides; i++) {
-        this.#drawLine(context, 1)
+        this.#drawLine(context, 0)
         context.rotate((Math.PI * 2) / this.sides)
       }
-
       context.restore()
     }
     #drawLine(context, level) {
       if (level > this.maxLevel) return
+
+      // inital line
       context.beginPath()
       context.moveTo(0, 0)
       context.lineTo(this.size, 0)
       context.stroke()
 
-      context.save()
-      context.translate(this.size, 0)
-      context.scale(0.5, 0.5)
-      context.rotate(0.5)
-      this.#drawLine(context, level + 1)
+      for (let i = 0; i < this.branches; i++) {
+        context.save()
+        context.translate(this.size, 0)
+        context.scale(this.scale, this.scale)
+
+        context.save()
+        context.rotate(this.angle)
+        this.#drawLine(context, level + 1)
+        context.restore()
+
+        context.save()
+        context.rotate(-this.angle)
+        this.#drawLine(context, level + 1)
+        context.restore()
+      }
+
       context.restore()
     }
   }
