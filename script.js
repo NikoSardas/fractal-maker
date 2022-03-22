c634bf478d5aaad292313634d6496740c35c3bc8
Niko Sardas <nikosardas.dev@gmail.com>
2022-03-22 14:15:54 -0400

    recursive shape inside a private method

--- script.js
+++ script.js
@@ -5,34 +5,44 @@ window.addEventListener('load', function () {
   canvas.width = 500
   canvas.height = 500
 
-  console.log(ctx)
-  ctx.lineWidth = 20
+  ctx.lineWidth = 8
   ctx.lineCap = 'round'
-  ctx.strokeStyle = 'orange'
+  ctx.strokeStyle = 'lightblue'
   ctx.fillStyle = 'blue'
 
   class Fractal {
     constructor(canvasWidth, canvasHeight) {
       this.canvasWidth = canvasWidth
       this.canvasHeight = canvasHeight
-      this.size = canvasWidth / 8
+      this.size = canvasWidth * 0.2
+      this.sides = 15
+      this.maxLevel = 10
     }
     draw(context) {
       context.save()
+      context.translate(this.canvasWidth / 2, this.canvasHeight / 2)
       context.scale(1, 1)
 
-      for (let i = 0; i < 10; i++) {
-        this.#drawLine(context)
-        context.rotate(0.3)
+      for (let i = 0; i < this.sides; i++) {
+        this.#drawLine(context, 1)
+        context.rotate((Math.PI * 2) / this.sides)
       }
 
       context.restore()
     }
-    #drawLine(context) {
+    #drawLine(context, level) {
+      if (level > this.maxLevel) return
       context.beginPath()
       context.moveTo(0, 0)
       context.lineTo(this.size, 0)
       context.stroke()
+
+      context.save()
+      context.translate(this.size, 0)
+      context.scale(0.5, 0.5)
+      context.rotate(0.5)
+      this.#drawLine(context, level + 1)
+      context.restore()
     }
   }
 
