fbd50e3a2dfd8e8d5705364e9de2229617712d43
Niko Sardas <nikosardas.dev@gmail.com>
2022-03-22 15:23:10 -0400

    snowflake

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
   canvas.width = 500
   canvas.height = 500
 
-  ctx.lineWidth = 30
+  ctx.lineWidth = 10
   ctx.lineCap = 'round'
   ctx.strokeStyle = 'lightblue'
   ctx.fillStyle = 'blue'
@@ -15,11 +15,11 @@ window.addEventListener('load', function () {
       this.canvasWidth = canvasWidth
       this.canvasHeight = canvasHeight
       this.size = canvasWidth * 0.2
-      this.sides = 4
-      this.maxLevel = 7
-      this.scale = 0.6
+      this.sides = 6
+      this.maxLevel = 3
+      this.scale = 0.4
       this.angle = 1
-      this.branches = 1
+      this.branches = 3
     }
     draw(context) {
       context.save()
@@ -43,7 +43,7 @@ window.addEventListener('load', function () {
 
       for (let i = 0; i < this.branches; i++) {
         context.save()
-        context.translate(this.size, 0)
+        context.translate(this.size - (this.size / this.branches) * i, 0)
         context.scale(this.scale, this.scale)
 
         context.save()
@@ -55,9 +55,8 @@ window.addEventListener('load', function () {
         context.rotate(-this.angle)
         this.#drawLine(context, level + 1)
         context.restore()
+        context.restore()
       }
-
-      context.restore()
     }
   }
 
