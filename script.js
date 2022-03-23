352e7c786387ef8e44c837fd11976880ee76e577
Niko Sardas <nikosardas.dev@gmail.com>
2022-03-22 22:00:49 -0400

    random snowflake

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ window.addEventListener('load', function () {
 
   ctx.lineWidth = 10
   ctx.lineCap = 'round'
-  ctx.strokeStyle = 'lightblue'
-  ctx.fillStyle = 'blue'
+
+  ctx.shadowColor = 'black'
+  ctx.shadowOffsetX = 5
+  ctx.shadowOffsetY = 10
+  ctx.shadowBlur = 20
 
   class Fractal {
     constructor(canvasWidth, canvasHeight) {
@@ -17,11 +20,13 @@ window.addEventListener('load', function () {
       this.size = canvasWidth * 0.2
       this.sides = 6
       this.maxLevel = 3
-      this.scale = 0.4
-      this.angle = 1
-      this.branches = 3
+      this.scale = 0.5
+      this.angle = Math.random() * 2.8 + 0.1
+      this.branches = 2
+      this.color = `hsl(${Math.random() * 360},25%,50%)`
     }
     draw(context) {
+      context.strokeStyle = this.color
       context.save()
       context.translate(this.canvasWidth / 2, this.canvasHeight / 2)
       context.scale(1, 1)
